00a17d8783928631d497006032a6b89c4faeeb1a
Lucila Alochis <lucila.alochis1999@gmail.com>
2023-03-01 23:39:24 -0300

    cambio de nombre arrays a tarjetas

--- main.js
+++ main.js
@@ -1,23 +1,23 @@
 const $section = document.getElementById("section")
 
-function crearTarjeta(array) {
+function crearTarjeta(tarjetas) {
     return ` 
      <div class="card mt-3 style=" width: 18rem;">
-     <img src="${array.image}" class="card-img-top imagenes" alt="cine">
+     <img src="${tarjetas.image}" class="card-img-top imagenes" alt="cine">
     <div class="card-body">
-        <h2>${array.name}</h2>
-        <p class="card-text">${array.description}</p>
+        <h2>${tarjetas.name}</h2>
+        <p class="card-text">${tarjetas.description}</p>
         <div class="d-flex justify-content-around">
-         <p><strong>Price: $</>    ${array.price}</strong><p/>
+         <p><strong>Price: $</>    ${tarjetas.price}</strong><p/>
             <a href="./details.html" class="btn btn-primary">Details</a>
         </div>
     </div>
     </div>`
 }
 
-function pintarTarjetas(array, elemento) {
+function pintarTarjetas(tarjetas, elemento) {
     let template = ''
-    for (let tarjeta of array) {
+    for (let tarjeta of tarjetas) {
         template += crearTarjeta(tarjeta)
     }
     elemento.innerHTML = template
